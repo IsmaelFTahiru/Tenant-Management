386728424d2df9bda558a4a5486a5b8f55efd83b
IsmaelFTahiru <ismael.tahiru@lifebonus.health>
2024-04-01 15:52:05 +0200

    add tdb manually

--- tenantManagement.js
+++ tenantManagement.js
@@ -1,6 +1,19 @@
 'use strict';
 //import { tenantDB } from './tenantManagementDB.js';
 
+const tenantDB = [
+  {
+    id: '33f58e25-be01-4b01-bb98-ea994aa9de85',
+    name: 'Lifebonus Development',
+    licenses: 10,
+  },
+  {
+    id: '187843b4-8beb-4349-b062-250a98ed719a',
+    name: 'Lifebonus Development 1',
+    licenses: 100,
+  },
+];
+
 const tenantSearchButton = document.getElementById('tenantSearchButton');
 const tenantSearch = document.getElementById('tenantSearch');
 
